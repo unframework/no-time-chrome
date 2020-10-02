deba8265141eb67d38656763d928fc44e993a548
Nick Matantsev <nick.matantsev@gmail.com>
2020-10-02 01:36:58 -0400

    Show/hide overlay depending on current Twitter path

--- src/contentTwitter.ts
+++ src/contentTwitter.ts
@@ -11,6 +11,7 @@ overlayNode.style.right = '0';
 overlayNode.style.bottom = '0';
 overlayNode.style.zIndex = '100'; // @todo is this enough?
 overlayNode.style.background = '#ffffff';
+overlayNode.style.opacity = '1';
 mainNode.appendChild(overlayNode);
 
 // location checker state
@@ -22,24 +23,24 @@ function checkLocationChange() {
   currentCheckAttemptCount += 1;
   const checkAttemptId = currentCheckAttemptCount;
 
-  async function waitAndCheck(attemptCount: number): Promise<boolean> {
+  async function waitAndCheck(attemptCount: number): Promise<string | null> {
     const timeoutMs = attemptCount * 20; // linear back-off for simplicity
     await new Promise((resolve) => setTimeout(resolve, timeoutMs));
 
     // bail out if a newer checker was launched
     if (currentCheckAttemptCount !== checkAttemptId) {
-      return false;
+      return null;
     }
 
-    // if there was a change, report it
+    // if there was a change, report the new locaiton
     if (document.location.href !== currentLocation) {
-      currentLocation = document.location.href;
-      return true;
+      currentLocation = document.location.href; // immediately mark this down in state
+      return currentLocation;
     }
 
     // bail out if we have been trying for too long
     if (attemptCount > 10) {
-      return false;
+      return null;
     }
 
     // otherwise, asynchronously recurse
@@ -47,9 +48,26 @@ function checkLocationChange() {
   }
 
   // always launch a fresh check sequence
-  waitAndCheck(0).then((locationChanged) => {
-    if (locationChanged) {
-      console.log('new location detected', currentLocation, checkAttemptId);
+  waitAndCheck(0).then((newLocation) => {
+    if (!newLocation) {
+      return;
+    }
+
+    const pathMatch = /^[^/]*\/\/[^/]*(\/.*)/.exec(newLocation);
+    const path = pathMatch && pathMatch[1];
+
+    if (!path) {
+      return;
+    }
+
+    if (path.startsWith('/home') || path.startsWith('/explore')) {
+      overlayNode.style.opacity = '1';
+      overlayNode.style.pointerEvents = 'auto'; // clicks are blocked
+      overlayNode.style.transition = 'none'; // immediate effect
+    } else {
+      overlayNode.style.opacity = '0';
+      overlayNode.style.pointerEvents = 'none'; // clicks fall through
+      overlayNode.style.transition = 'opacity 0.2s ease-out'; // fade out
     }
   });
 }
